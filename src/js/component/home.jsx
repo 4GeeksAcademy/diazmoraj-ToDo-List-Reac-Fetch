4a0447a1b7cd0e851a8de05fd0e3a3bc91675faf
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-06-05 03:39:47 +0000

    Use Effect

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,64 +4,38 @@ import React, { useEffect, useState } from "react";
 //create your first component
 const Home = () => {
 
-	const todosURL = "https://playground.4geeks.com/todo/users/diazmoraj"
-
-	fetch(todosURL)
-	.then((response) => {console.log(response)
-		return response.json()
-	})
-	.then((data) => {console.log(data)})
-	.catch((error) => {error})
-
-	/*const [todos, setTodos] = useState([])
+	const getTodosURL = "https://playground.4geeks.com/todo/users/diazmoraj"
+	const addTodosURL = "https://playground.4geeks.com/todo/todos/diazmoraj"
+	const [todos, setTodos] = useState([])
 
 	useEffect(() => {
-		getTodos();
-	},[]);
+		fetch(getTodosURL)
+		.then(response => response.json())
+		.then(data => {setTodos(data.todos)})
+		.catch((error) => {error})
+	}, [])
 	
-	//obtener lista de todos
-	const getTodos = () => {
+	let newTodo ={
+		"label": "jugar bola"
+	}
 
-		fetch(todosURL + 'users/diazmoraj')
-		.then(response=>response.json())
-		.then(data => {
-			if (Array.isArray(data.todos)){
-				setTodos(data.todos);
-			}else{
-				console.error('La respuesta no es un array', data);
-			}
-		})
-		.catch((error) => {console.log(error)});
-	};
-
-	
-
-	// obtener lista de todos
-	function addTodos(){
-		let newTodo ={}
-		fetch(todosURL + 'users/diazmoraj', {
+	function addTodos() {
+		fetch(addTodosURL, {
 			method: "POST",
 			body: JSON.stringify(newTodo),
 			headers: {'Content-type': 'application/json'}
 		})
 		.then(response=>response.json())
-		.then(data=>{
-			setTodos(data.todos)
-		})
-		.catch((error) => {console.log(error)})
+		.then(data=> console.log(data))
+		.catch((error) => (error))
 	}
-		
-	function deltodos(){
-		
-	}
-	getTodos();*/
-
-
-
+	
 	return (
 		<div className="text-center">
 			<h1>TO DO's</h1>
-			<h3>{getTodos}</h3>
+			{todos.map((value, index) => {
+				return <h3 key={index}>{value.label}</h3>
+			})}
 		</div>
 	);
 };
