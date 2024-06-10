502aebe858f33570ec16af6db0557f6d03270fbb
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-06-10 15:18:30 +0000

    delete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
 	const getTodosURL = "https://playground.4geeks.com/todo/users/diazmoraj"
 	const addTodosURL = "https://playground.4geeks.com/todo/todos/diazmoraj"
 	const delTodosURL = "https://playground.4geeks.com/todo/todos/"
+	const [inputValue, setInputValue] = useState("")
 	const [todos, setTodos] = useState([])
 
 	useEffect(() => {
@@ -43,9 +44,24 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<h1>TO DO's</h1>
+			<input
+				type="text"
+				onChange={(e) => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyDown={(enter) => {
+					if (enter.key == "Enter"){
+						if
+							(inputValue.trim() !==""){
+								setTodos(todos.concat([inputValue.trim()]));
+								setInputValue("");
+							}
+						}
+					}}
+						placeholder="What do you need to do"></input>
 			{todos.map((value, index) => {
 				return <h3 key={index}>{value.label}</h3>
 			})}
+			<button className="btn btn-danger" onClick={delTodos}>Delete</button>
 		</div>
 	);
 };
